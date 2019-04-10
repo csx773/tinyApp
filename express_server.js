329d97a86a89d4b020d3017e8b3791f9e82d587b
Chris Xiao <csx773@gmail.com>
2019-04-10 01:07:05 +0000

    added: url_new page and handle GET and POST from form

--- express_server.js
+++ express_server.js
@@ -3,12 +3,21 @@ var express = require("express");
 var app = express();
 var PORT = 8080;
 
+//init the body-parser module
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+//init EJS module
 app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//genreate unique URL
+function generateRandomString() {
+
+}
 
 //event Handler for differnt routes
 app.get("/", (req, res) => {
@@ -30,11 +39,21 @@ app.get("/urls", (req, res) => {
   //in .ejs file, onject key name will be: urls
   res.render("urls_index", templateVars);
 });
+//handdle GET for new tiny URL
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL,
                         longURL: urlDatabase };
   res.render("urls_show", templateVars);
+});
+
+//event Handler for POST from browser
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok for now, longURL recieved");         // Respond with 'Ok' (we will replace this)
 });
 
 //start the server to listen to requests
